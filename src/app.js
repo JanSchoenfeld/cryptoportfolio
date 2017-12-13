bf4b295db0dcda9d1871ece48659aab6a49617c4
JanSchoenfeld <jan.schoenfeld@fh-muenster.de>
2017-12-13 20:18:22 +0100

    minor changes + express

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 var fs = require("fs");
+var express = require("express");
+const exphbs = require('express-handlebars');
+
+var app = express();
 
 var portfolio = JSON.parse(fs.readFileSync("../portfolio.json"));
 
 for (let balance of portfolio){
     console.log(balance.name + " " + balance.balance);
 }
+
+app.get("/", function(req, res){
+
+    res.send("Hello Blyets");
+    res.render
+
+});
+
+app.listen(3000, function(){
+    
+    console.log("server listening blyet");
+
+});
 
