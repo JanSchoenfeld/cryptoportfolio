f6a4787de0236ba62662fb1b18fbd57598a315cd
JanSchoenfeld <jan.schoenfeld@fh-muenster.de>
2017-12-14 22:41:31 +0100

    call coindesk api for btc price in usd and eur

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ var fs = require("fs");
 var express = require("express");
 const exphbs = require('express-handlebars');
 const path = require('path');
+var request = require('request');
 
 var app = express();
+var coindeskResponse;
 
 const engineConfig = {
     extname: '.hbs', defaultLayout: 'main',
@@ -16,11 +18,26 @@ app.set('view engine', 'hbs');
 
 var portfolio = JSON.parse(fs.readFileSync("../portfolio.json"));
 
+var request = require("request");
+
+var options = { method: 'GET',
+  url: 'https://api.coindesk.com/v1/bpi/currentprice/EUR.json'};
+
+
+
+
 
 app.get("/", function(req, res){
 
     //hier die views datei einfügen die in main.hsb gerendert werden soll
-    res.render('portfolio', {portfolio});
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        coindeskResponse = JSON.parse(body);
+        console.log(coindeskResponse.bpi.USD.rate_float);
+        console.log(coindeskResponse.bpi.EUR.rate_float);
+        res.render('portfolio', {portfolio:portfolio, bitcoinPrice: coindeskResponse.bpi.USD.rate_float});
+      });
 
 });
 
